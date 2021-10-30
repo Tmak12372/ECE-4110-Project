5e27e2f7628d18d5becdce2dcc46c70c3bde59b8
Tmak12372 <77301911+Tmak12372@users.noreply.github.com>
2021-10-30 17:30:19 -0500

    Added logo but not in correct position

--- reorg/proj0/hud.vhd
+++ reorg/proj0/hud.vhd
@@ -46,17 +46,17 @@ architecture rtl of hud is
     constant c_bar_height : integer := 3;
     constant c_upper_bar_pos : integer := 30 - c_bar_height;
     constant c_lower_bar_pos : integer := g_screen_height - 30;
-
+	 
     constant c_ship_spacing_x : integer := 10;
-    constant c_ship_pos_y : integer := c_upper_bar_pos/2 - g_ship_height/2;
-    constant c_ship_pos_x1 : integer := 20;
-    constant c_ship_pos_x2 : integer := c_ship_pos_x1 + 1*(g_ship_width + c_ship_spacing_x);
-    constant c_ship_pos_x3 : integer := c_ship_pos_x1 + 2*(g_ship_width + c_ship_spacing_x);
-    constant c_ship_pos_x4 : integer := c_ship_pos_x1 + 3*(g_ship_width + c_ship_spacing_x);
-    constant c_ship_pos_x5 : integer := c_ship_pos_x1 + 4*(g_ship_width + c_ship_spacing_x);
+    constant c_ship_pos_y   : integer := c_upper_bar_pos/2 - g_ship_height/2;
+    constant c_ship_pos_x1  : integer := 20;
+    constant c_ship_pos_x2  : integer := c_ship_pos_x1 + 1*(g_ship_width + c_ship_spacing_x);
+    constant c_ship_pos_x3  : integer := c_ship_pos_x1 + 2*(g_ship_width + c_ship_spacing_x);
+    constant c_ship_pos_x4  : integer := c_ship_pos_x1 + 3*(g_ship_width + c_ship_spacing_x);
+    constant c_ship_pos_x5  : integer := c_ship_pos_x1 + 4*(g_ship_width + c_ship_spacing_x);
 
-    constant c_char_width : integer := 8;
-    constant c_char_height : integer := 16;
+    constant c_char_width   : integer := 8;
+    constant c_char_height  : integer := 16;
     constant c_char_spacing : integer := 0;
     constant c_score_pos_x1 : integer := g_screen_width - 6*(c_char_width + c_char_spacing) - 5;
     constant c_score_pos_x2 : integer := c_score_pos_x1 + 1*(c_char_width + c_char_spacing);
@@ -64,8 +64,18 @@ architecture rtl of hud is
     constant c_score_pos_x4 : integer := c_score_pos_x1 + 3*(c_char_width + c_char_spacing);
     constant c_score_pos_x5 : integer := c_score_pos_x1 + 4*(c_char_width + c_char_spacing);
     constant c_score_pos_x6 : integer := c_score_pos_x1 + 5*(c_char_width + c_char_spacing);
-    constant c_score_pos_y : integer := c_upper_bar_pos/2 - c_char_height/2;
+    constant c_score_pos_y  : integer := c_upper_bar_pos/2 - c_char_height/2;
 
+	 constant c_logo_pos_x1 : integer := g_screen_width - 6*(c_char_width + c_char_spacing) - 5;
+    constant c_logo_pos_x2 : integer := c_logo_pos_x1 + 1*(c_char_width + c_char_spacing);
+    constant c_logo_pos_x3 : integer := c_logo_pos_x1 + 2*(c_char_width + c_char_spacing);
+    constant c_logo_pos_x4 : integer := c_logo_pos_x1 + 3*(c_char_width + c_char_spacing);
+    constant c_logo_pos_x5 : integer := c_logo_pos_x1 + 4*(c_char_width + c_char_spacing);
+    constant c_logo_pos_x6 : integer := c_logo_pos_x1 + 5*(c_char_width + c_char_spacing);
+    constant c_logo_pos_y  : integer := c_upper_bar_pos/2 - c_char_height/2;
+	 
+	 
+	 
     -- Components
     component triangle is
         port (
@@ -104,6 +114,20 @@ architecture rtl of hud is
         );
     END component;
 
+	 
+	 component alphabetical_font is
+    port (
+        -- Value of the char
+        i_value : in integer;
+
+        -- Pixel index for the character
+        i_row : in integer; -- 0 to height-1
+        i_column : in integer; -- 0 to width-1
+
+        o_draw : out std_logic
+    );
+	end component;
+	
     -- Signals
     signal w_ship1_draw : std_logic;
     signal w_ship2_draw : std_logic;
@@ -121,12 +145,17 @@ architecture rtl of hud is
 
     signal r_curr_score_digit : integer := 0;
     signal r_font_val : integer := 0;
-    
+	 
+	 signal w_alphabetical_font_draw : std_logic;
+	 signal r_curr_logo_digit : integer := 0;
+	 signal r_alphabetical_font_val : integer := 0;
+    signal r_alphabetical_font_row : integer := 0;
+    signal r_alphabetical_font_column : integer := 0;
 begin
     -- Concurrent assignments
     r_font_row <= i_row - c_score_pos_y;
     r_score_slv <= std_logic_vector(to_unsigned(i_score, r_score_slv'length));
-
+	 r_alphabetical_font_row <= i_row - c_logo_pos_y;
     
 
     -- Which digit of score?
@@ -183,8 +212,64 @@ begin
         end case;
     end process;
     
+	 
+	 -- Which digit of logo?
+    process(i_row, i_column)
+    begin
+        if (i_row >= c_logo_pos_y and i_row < c_logo_pos_y + c_char_height) then
+
+            if (i_column >= c_logo_pos_x1 and i_column < c_logo_pos_x1 + c_char_width) then
+                r_curr_logo_digit <= 1;
+            elsif (i_column >= c_logo_pos_x2 and i_column < c_logo_pos_x2 + c_char_width) then
+                r_curr_logo_digit <= 2;
+            elsif (i_column >= c_logo_pos_x3 and i_column < c_logo_pos_x3 + c_char_width) then
+                r_curr_logo_digit <= 3;
+            elsif (i_column >= c_logo_pos_x4 and i_column < c_logo_pos_x4 + c_char_width) then
+                r_curr_logo_digit <= 4;
+            elsif (i_column >= c_logo_pos_x5 and i_column < c_logo_pos_x5 + c_char_width) then
+                r_curr_logo_digit <= 5;
+            elsif (i_column >= c_logo_pos_x6 and i_column < c_logo_pos_x6 + c_char_width) then
+                r_curr_logo_digit <= 6;
+            else
+                r_curr_logo_digit <= 0;
+            end if;
+
+        else
+            r_curr_logo_digit <= 0;
+        end if;
+    end process;
+	 
+	     -- Pick out the correct alphabetic value to display from ROM
+    process(r_curr_logo_digit,i_column)
+    begin
+        case r_curr_score_digit is
+            when 1 =>
+                r_alphabetical_font_val <= 19; --T
+                r_alphabetical_font_column <= i_column - c_logo_pos_x1;
+            when 2 => 
+                r_alphabetical_font_val <= 13; --N
+                r_alphabetical_font_column <= i_column - c_logo_pos_x2;
+            when 3 => 
+                r_alphabetical_font_val <= 19; --T
+                r_alphabetical_font_column <= i_column - c_logo_pos_x3;
+            when 4 => 
+                r_alphabetical_font_val <= 4; --E
+                r_alphabetical_font_column <= i_column - c_logo_pos_x4;
+            when 5 => 
+                r_alphabetical_font_val <= 2;  --C
+                r_alphabetical_font_column <= i_column - c_logo_pos_x5;
+            when 6 => 
+                r_alphabetical_font_val <= 7;  --H
+                r_alphabetical_font_column <= i_column - c_logo_pos_x6;
+            when others =>
+                r_alphabetical_font_val <= 0;
+                r_alphabetical_font_column <= 0;
+        end case;
+    end process;
+	 
+	 
     -- Set draw output
-    process(i_row, i_column, w_font_draw)
+    process(i_row, i_column, w_font_draw, w_alphabetical_font_draw)
         variable r_draw_tmp : std_logic := '0';
         variable r_color_tmp : integer range 0 to 4095 := 0;
     begin
@@ -213,7 +298,11 @@ begin
         end if;
 
         -- Logo
-        
+        if (r_curr_logo_digit /= 0 and w_alphabetical_font_draw = '1') then
+		      r_draw_tmp := '1';
+            r_color_tmp := g_logo_color;
+        end if;
+		  
         -- Assign outputs
         o_draw <= r_draw_tmp;
         o_color <= r_color_tmp;
@@ -243,7 +332,10 @@ begin
     ship5 : triangle port map (i_row => i_row, i_column => i_column, i_xPos => c_ship_pos_x5, i_yPos => c_ship_pos_y, o_draw => w_ship5_draw);
 
     digits_font : font port map (i_value => r_font_val, i_row => r_font_row, i_column => r_font_column, o_draw => w_font_draw);
-    bcdconv : binary_to_bcd generic map(
+	 
+	 alpha_font  : alphabetical_font port map (i_value => r_alphabetical_font_val, i_row => r_alphabetical_font_row, i_column => r_alphabetical_font_column, o_draw => w_alphabetical_font_draw);
+    
+	 bcdconv : binary_to_bcd generic map(
             bits => 20,
             digits => 6
         )
